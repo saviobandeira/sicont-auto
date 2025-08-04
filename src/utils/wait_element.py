a4b09e8cb8aad09f8d53105e36025c60b02583bf
saviobandeira <savio-bandeira@hotmail.com>
2025-08-04 11:47:48 -0300

    feat: add wait_element_presence

--- src/utils/wait_element.py
+++ src/utils/wait_element.py
@@ -3,3 +3,6 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def wait_element_visible(driver, locator, timeout=10):
     return WebDriverWait(driver, timeout).until(EC.visibility_of_element_located(locator))
+
+def wait_element_presence(driver, locator, timeout=10):
+    return WebDriverWait(driver, timeout).until(EC.presence_of_element_located(locator))
